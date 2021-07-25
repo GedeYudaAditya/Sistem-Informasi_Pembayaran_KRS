af08d4d7ed97ef31cbffee22b358a3ace03b0ffb
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2021-07-25 15:08:07 +0800

    perbaikan tambahan

---
+++ application/controllers/Krs.php
@@ -0,0 +1,42 @@
+<?php
+class Krs extends CI_Controller
+{
+
+    public function index()
+    {
+        if (!$this->ion_auth->logged_in() || !$this->ion_auth->in_group(group)) {
+            redirect('krs');
+        } else {
+            $this->data['title'] = "KRS - Data Mahasiswa";
+            $this->data['active'] = "6";
+            $id = $_SESSION['user_id'];
+            $this->data['flip'] = "false";
+            $this->data['ckeditor'] = "krs";
+
+            $this->data['group'] = $this->ion_auth_model->getGroup($id);
+
+            $this->load->view("admin/master/header", $this->data);
+            $this->load->view("admin/page/krs/index", $this->data);
+            $this->load->view("admin/master/footer", $this->data);
+        }
+    }
+
+    public function tambah_Mahasiswa()
+    {
+        if (!$this->ion_auth->logged_in() || !$this->ion_auth->in_group(group)) {
+            redirect('krs');
+        } else {
+            $this->data['title'] = "KRS - Tambah Data Mahasiswa";
+            $this->data['active'] = "6";
+            $id = $_SESSION['user_id'];
+            $this->data['flip'] = "false";
+            $this->data['ckeditor'] = "krs";
+
+            $this->data['group'] = $this->ion_auth_model->getGroup($id);
+
+            $this->load->view("admin/master/header", $this->data);
+            $this->load->view("admin/page/krs/tambah", $this->data);
+            $this->load->view("admin/master/footer", $this->data);
+        }
+    }
+}
