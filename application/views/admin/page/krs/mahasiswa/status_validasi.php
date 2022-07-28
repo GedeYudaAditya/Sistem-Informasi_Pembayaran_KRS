9433a05cde1fd3d598754057b206aa832831515f
losinanda <kadekprawira29@gmail.com>
2022-07-28 22:14:54 +0800

    membuat tampilan validasi

---
+++ application/views/admin/page/krs/mahasiswa/status_validasi.php
@@ -0,0 +1,134 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-3 text-gray-800">Status Validasi</h1>
+        <div class="card shadow mb-3">
+            <!-- Card Header - Accordion -->
+            <a href="#" class="d-block card-header py-3" data-toggle="collapse" role="button"
+                aria-expanded="true" aria-controls="tampilan_validasi">
+                <h6 class="m-0 font-weight-bold text-primary">Detail Informasi</h6>
+            </a>
+            <!-- Card Content - Collapse -->
+            <div class="collapse show" id="nim" data-parent="#tampilan_validasi">
+                <div class="card-body row justify-content-center text-center p-1 mt-3">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="col-lg-12 mb-3">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body p-0">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">NIM</div>
+                                            <div class="p mb-0 font-weight-bold text-gray-800">
+                                                <?php echo "TEST" ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="collapse show" id="nama" data-parent="#tampilan_validasi">
+                <div class="card-body row justify-content-center text-center p-1">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="col-lg-12 mb-3">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body p-0">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Nama</div>
+                                            <div class="p mb-0 font-weight-bold text-gray-800">
+                                                <?php echo "TEST" ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="collapse show" id="prodi" data-parent="#tampilan_validasi">
+                <div class="card-body row justify-content-center text-center p-1">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="col-lg-12 mb-3">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body p-0">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Program Studi</div>
+                                            <div class="p mb-0 font-weight-bold text-gray-800">
+                                                <?php echo "TEST" ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="collapse show" id="thn_ajaran" data-parent="#tampilan_validasi">
+                <div class="card-body row justify-content-center text-center p-1">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="col-lg-12 mb-3">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body p-0">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Tahun Ajaran</div>
+                                            <div class="p mb-0 font-weight-bold text-gray-800">
+                                                <?php echo "TEST" ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="collapse show" id="smstr" data-parent="#tampilan_validasi">
+                <div class="card-body row justify-content-center text-center p-1">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="col-lg-12 mb-3">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body p-0">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Semester</div>
+                                            <div class="p mb-0 font-weight-bold text-gray-800">
+                                                <?php echo "TEST" ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="collapse show" id="kegiatan" data-parent="#tampilan_validasi">
+                <div class="card-body row justify-content-center text-center p-1">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="col-lg-12 mb-3">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body p-0">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">File Validasi</div>
+                                            <div class="p mb-0 font-weight-bold text-gray-800">
+                                                <?php echo "TEST" ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="collapse show" id="kegiatan" data-parent="#tampilan_validasi">
+            <div class="card-body row justify-content-center text-center">
+                <div class="font-weight-bold text-primary text-center">STATUS: <div class="alert alert-success" role="alert"> VALID </div> 
+            </div>
+            </div>
+        </div>
+        </div>
+</div>
