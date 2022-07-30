6e8bc09ab2bf6d5d4ab3c0a5eeaa72e8b18d2dae
adisastrawan <adisastrawan110703@gmail.com>
2022-07-30 11:01:47 +0800

    minor changes

--- application/views/admin/page/krs/dosen/viewValidasiMahasiswa.php
+++ application/views/admin/page/krs/dosen/viewValidasiMahasiswa.php
@@ -33,7 +33,7 @@
                 </tr>
               </thead>
               <tbody >
-                <?php $i=0?>
+                <?php $i=1?>
                 <?php foreach($value as $mhs) : ?>
                 <tr>
                   <td>
