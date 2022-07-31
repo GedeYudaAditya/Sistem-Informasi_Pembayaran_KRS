8b15d8a05167150e3b1885af1692818b23fa01f7
adisastrawan <adisastrawan110703@gmail.com>
2022-07-31 16:39:36 +0800

    commit

--- application/controllers/Krs.php
+++ application/controllers/Krs.php
@@ -533,9 +533,9 @@ class Krs extends CI_Controller
         $where = array('user_id' => $id);
         $dosen_id['pa_id']= $this->All_model->findDosen($where)->result_array();
         print_r($dosen_id);
-        $where['pa_id'] = $dosen_id['pa_id'][0]['id'];
-        print_r($where);
-        $mahasiswa['value'] = $this->All_model->gatherData($where)->result();
+        $find['pa_id'] = $dosen_id['pa_id'][0]['id'];
+        print_r($find);
+        $mahasiswa['value'] = $this->All_model->gatherData($find)->result();
 		$this->load->view("admin/master/header", $this->data);
 		$this->load->view("admin/page/krs/dosen/viewValidasiMahasiswa",$mahasiswa);
 		$this->load->view("admin/master/footer", $this->data);
