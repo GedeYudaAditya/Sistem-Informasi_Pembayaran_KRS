7714b8d7b1169b9a75a0fff5824b7b42f37240e8
adisastrawan <adisastrawan110703@gmail.com>
2022-07-31 11:38:34 +0800

    finding dosen

--- application/controllers/Krs.php
+++ application/controllers/Krs.php
@@ -531,8 +531,7 @@ class Krs extends CI_Controller
 		$id = $_SESSION['user_id'];
 		$this->data['group'] = $this->ion_auth_model->getGroup($id);
         $dosen_id= $this->All_model->findDosen($id)->result();
-        $where = array('id'=>$dosen_id);
-        $mahasiswa['value'] = $this->All_model->gatherData($where)->result();
+        $mahasiswa['value'] = $this->All_model->gatherData($dosen_id)->result();
 		$this->load->view("admin/master/header", $this->data);
 		$this->load->view("admin/page/krs/dosen/viewValidasiMahasiswa",$mahasiswa);
 		$this->load->view("admin/master/footer", $this->data);
