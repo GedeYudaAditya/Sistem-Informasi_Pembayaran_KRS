af28cd3f055d66d96292f507e97fb6a015e826f0
marselinusharson <marselinus@undiksha.ac.id>
2022-12-25 22:32:59 +0800

    update link source img bukti_iuran dari Folder_KRS

--- application/views/admin/page/krs/admin/formIuran.php
+++ application/views/admin/page/krs/admin/formIuran.php
@@ -8,18 +8,13 @@
       <form action="<?= base_url(); ?>krs/simpanIuran" method="POST">
 
 
-        <div class="d-md-flex  justify-content-between pt-4">
+        <div class="row d-md-flex  justify-content-between pt-4">
           <!-- Start Input Tahun Ajaran -->
           <div class="col-12 col-md-6">
-            <label class="d-block pl-3" for="expireDate">Tahun Ajaran</label>
+            <label class="d-block pl-3" for="Tahun">Tahun Ajaran</label>
             <div class="form-group input-group mb-3 d-flex">
               <div class="col-12 col-md-5 input-group mb-3">
                 <input type="number" placeholder="Tahun" class="form-control" id="inputTahunDepan" name="inputTahunDepan" onchange="inputTahunDosen()">
-                <!-- <input type="number" placeholder="YYYY" min="1999" max="2023" name="tahun_ajaran" id="tahun_ajaran">
-                <script>
-                  document.querySelector("input[type=number]")
-                    .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1))
-                </script> -->
               </div>
               <div class="d-none d-md-block col-md-1" style="font-size: 25px;">/</div>
               <input id="inputTahunBelakang" name="inputTahunBelakang" class="col-12 col-md-5 form-control " type="number" placeholder="..." readonly>
@@ -45,6 +40,7 @@
           <!-- Start Input Semester -->
           <div class="col-12 col-md-6">
             <div>
+              <label class="d-block pl-1" for="Semester">Semester</label>
               <div class="form-group input-group mb-3">
                 <select class="custom-select" name="semester" id="inputGroupSelect01">
                   <option selected disabled>Pilih Semester</option>
@@ -56,7 +52,8 @@
           </div>
           <!-- End Input Semester -->
         </div>
-        <div class="d-flex justify-content-end">
+        <div class="row d-flex justify-content-between">
+          <div></div>
           <button type="submit" name="buatForm" class="btn btn-primary justify-content-right font-weight-bold my-3">Tambah Iuran</button>
         </div>
       </form>
